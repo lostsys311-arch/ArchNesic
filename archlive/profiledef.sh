451f08762b60bb7848acb033c10eb119e07d217a
olafkow <olafkow@users.noreply.github.com>
2026-07-07 20:36:44 +0100

    fix: remove deprecated bootmodes, drop community repo, fix permissions

--- archlive/profiledef.sh
+++ archlive/profiledef.sh
@@ -9,18 +9,14 @@ iso_version="$(date +%Y.%m.%d)"
 install_dir="awipe"
 buildmodes=('iso')
 bootmodes=(
-  'bios.syslinux.mbr'
-  'bios.syslinux.eltorito'
-  'uefi-x64.systemd-boot.esp'
-  'uefi-x64.systemd-boot.eltorito'
+  'bios.syslinux'
+  'uefi.systemd-boot'
 )
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '22')
 file_permissions=(
-  ["/etc/shadow"]="0:0:400"
-  ["/etc/gshadow"]="0:0:400"
   ["/root"]="0:0:700"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/usr/local/bin/amnesic-init.sh"]="0:0:755"
